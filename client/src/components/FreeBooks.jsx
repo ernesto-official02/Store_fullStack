80c6a48fe2980e824953859c6951335d05d48a6c
Ernest Anmol <167057735+ernesto-official02@users.noreply.github.com>
2025-09-09 21:20:43 +0530

    Update FreeBooks.jsx

--- client/src/components/FreeBooks.jsx
+++ client/src/components/FreeBooks.jsx
@@ -13,7 +13,7 @@ function FreeBooks() {
   useEffect(()=>{
     const getBook =async()=>{
       try{
-      const res= await axios.get("https://bookstore01.onrender.com");
+      const res= await axios.get("https://bookstore01.onrender.com/book");
       const data=res.data.filter((data) => data.category === "Free")
       console.log(data)
       setBook(data)
